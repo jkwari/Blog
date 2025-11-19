a0343e5fe0ec67ba35089ef7cc6ff4b21275c038
Jamal <warijamalk@gmail.com>
2025-11-19 21:30:13 +0300

    Adding express-validator to the project

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,21 +1,44 @@
+const { validationResult } = require("express-validator");
+
 exports.getFeed = (req, res, next) => {
   res.status(200).json({
-    title: "Something",
-    author: "Jamal Eldeen Wari",
-    tableOfContent: ["introduction", "Theory", "Technical", "Practical"],
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post",
+        imageUrl: "images\rdr2.jpg",
+        creator: {
+          name: "Jamal Eldeen Wari",
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // 422 is the validation failed status code
+    return res.status(422).json({
+      message: "Validation Failed in the Server side",
+      errors: errors.array(),
+    });
+  }
   const title = req.body.title;
-  const author = req.body.author;
+  const content = req.body.content;
 
   res.status(201).json({
     message: "Post Added Successfully !!!",
     post: {
-      id: new Date().toISOString(),
+      _id: new Date().toISOString(),
       title: title,
-      author: author,
+      content: content,
+      creator: {
+        name: "Jamal Eldeen Wari", // this will be dynamic later not like this static
+      },
+      createdAt: new Date(),
     },
   });
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { body } = require("express-validator");
+
 const router = express.Router();
 
 const feedController = require("../controllers/feed");
@@ -8,6 +10,15 @@ const feedController = require("../controllers/feed");
 router.get("/posts", feedController.getFeed);
 
 // POST: /feed/addPost
-router.post("/addPost", feedController.createPost);
+router.post(
+  "/addPost",
+  [
+    // Here we want to validate both title and content from the server side and the validation
+    // must match the client side in terms of length which is 5 for both frontend and backend
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.createPost
+);
 
 module.exports = router;
