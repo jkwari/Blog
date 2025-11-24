9756d9e628c9367ec76c6459299b72ff60ca6dbd
Jamal <warijamalk@gmail.com>
2025-11-24 23:58:44 +0300

    Adding authentication with JWT but Not Done

---
+++ controllers/auth.js
@@ -0,0 +1,99 @@
+const User = require("../models/user");
+
+const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
+
+require("dotenv").config();
+
+const bcrypt = require("bcrypt");
+
+exports.signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error("Validation Failed");
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+
+  // Here if the validation is successfull then we extract the fields from body and
+  // store it in the database.
+
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+
+  // We need to hash the paasword before storing it in the database
+  bcrypt
+    .hash(password, 12)
+    .then((hasedPass) => {
+      const addUser = new User({
+        email: email,
+        password: hasedPass,
+        name: name,
+      });
+      return addUser.save();
+    })
+    .then((result) => {
+      res
+        .status(201)
+        .json({ message: "User Added Successfully", post: result });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+    });
+};
+
+exports.login = (req, res, next) => {
+  // Extract the email and password from the body
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Check if that email we extracted is in the database
+
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error("User does not Exist");
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      // if the user is found then we need to check if the extracted password matches the one in the database
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error("Password does not match");
+        error.statusCode = 401;
+        throw error;
+      }
+      // Set up JWT next.....
+      // Here we store some information regarding the user but not password,
+      // because it can be decoded and the information will be exposed
+      const payload = {
+        email: loadedUser.email,
+        userId: loadedUser._id.toString(),
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "1h", // We don't want the token always available for security purposes after one hour the token will be expired
+      });
+      // This response will be sent to frontend and the token will be stored in a local storage,
+      // to make sure any action the user does from now on he/she must be authenticated
+      res.status(200).json({
+        message: "The user is autheticated using JWT",
+        token: token,
+        userId: loadedUser._id.toString(),
+      });
+    })
+    .catch((error) => {
+      if (!error.statusCode) {
+        error.statusCode = 500;
+      }
+      next(error);
+    });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,36 @@
 const express = require("express");
+const { body } = require("express-validator");
 
+const User = require("../models/user");
+
+const authController = require("../controllers/auth");
 const router = express.Router();
 
-// We can use PUT here because Put can be used to create a user once and also overwrite that user
-router.put("/signup");
+// We can use PUT here because Put can be used to create a user once and also overwrite that user.
+router.put(
+  "/signup",
+  [
+    body("email")
+      .isEmail() // Checking if the provided email in put body is an actual email written correctlly.
+      .withMessage("Please Provide a Valid Email") // This is a message for user if written wrong.
+      .custom((value, { req }) => {
+        // In this custom function we want to check in the database if the provided email is in the database or not.
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            //Checks the database to see if the provided email already exists. If it exists, reject validation by returning a rejected Promise.
+            return Promise.reject("This Email Already Exists!!!");
+          }
+        });
+      })
+      .normalizeEmail(), // Converts the email to normalized form by removing extra dots, converting domain to lowercase, and fixing common formating issues.
+    //  so all emails have the same format when they are stored in the database.
+
+    body("password").trim().isLength({ min: 8 }), // trim(): does remove any white spaces
+    body("name").trim().not().isEmpty(), // not().isEmpty(): Makes sure this field is not empty and the validation will fail if it is
+  ],
+  authController.signup
+);
+
+router.post("/login", authController.login);
 
 module.exports = router;
